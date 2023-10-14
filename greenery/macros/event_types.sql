2328611534a81d377de6a717e58ca0c5a5b25456
kat jakel <katinajakel@gmail.com>
2023-10-14 01:02:43 +0000

    week 3

--- greenery/macros/event_types.sql
+++ greenery/macros/event_types.sql
@@ -1,14 +1,14 @@
-{%- macro event_processor(table_name, column_name) -%} 
+{%- macro event_types(table_name,column_name) -%}
 
-{%-
-set event_types = dbt_utils.get_column_values(
-    table = ref(table_name)
-    , column = column_name
-)
--%} 
-    
-{%- for event in event_types %}
-   , sum(case when event_type = '{{event}}' then 1 else 0 end) as is_{{event}}
-{%- endfor %}
+{%- 
+    set event_types = dbt_utils.get_column_values(
+        table = ref(table_name)
+        , column = column_name
+    )
+-%}
 
-{% endmacro %} 
+    {%- for event_type in event_types -%}
+    , sum(case when event_type = '{{event_type}}' then 1 else 0 end) as {{event_type}}s
+    {% endfor %}
+
+{%- endmacro -%}
